4698266681a211de5f1fa47853b0edcd853c65a8
ashikr7964-create <ashikr7964@gmail.com>
2025-12-04 15:05:11 +0530

    Add files via upload
    day2

---
+++ learndatatypes.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class learndatatypes {
+	
+	public static void main(String[] args) {
+	   int number = 20;
+	   char car = 'P';
+	    String name = "prathap";
+	      
+	    boolean value = true;
+	    
+	    System.out.println(value);
+	}
+
+
+}
